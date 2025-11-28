0fd3bb1efdbfd31fdbe7f808854b810165779f2d
lucasferraz122 <lucasfersan10@gmail.com>
2025-11-28 03:36:12 -0300

    Update tabelahashrecuperacao.java
    descobri que acabei não seguindo 100% das regras então ajustei o codigo

--- tabelahashrecuperacao.java
+++ tabelahashrecuperacao.java
@@ -26,41 +26,42 @@ public class tabelahashrecuperacao {
     private static final long seed2 = 271828;
     private static final long seed3 = 314159;
 
-    private static final int FUNC_DIV = 0;
-    private static final int FUNC_MUL = 1;
-    private static final int FUNC_FOLD = 2;
+    private static final int TIPO_DIV = 0;
+    private static final int TIPO_MUL = 1;
+    private static final int TIPO_FOLD = 2;
 
     private No[] tabela;
-    private int tamanhoTabela;
+    private int tamanhoM;
 
     // variaveis pra guardar as contagens de tempo e tambem de colisoa
-    private long totalColisoesTabela;
-    private long totalColisoesLista;
-    private long somaHashPrimeiros10;
-    private int contadorInsercao;
-    private long comparacoesHits;
-    private long comparacoesMisses;
+    private long colsTabela;
+    private long colsLista;
+    private long somaCheck;
+    private int contInsercao;
+
+    // metricas que eu usei pra fazer a busca
+    private long cmpHits;
+    private long cmpMisses;
 
     // divisao resto da divisao pelo tamanho
-    private int hashDivisao(int k) {
-        int idx = k % this.tamanhoTabela;
+    private int hDiv(int k) {
+        int idx = k % this.tamanhoM;
         if (idx < 0) idx = idx * -1;
         return idx;
     }
 
     // multiplicacao usando a constante aurea que o enunciado deu
-    private int hashMultiplicacao(int k) {
+    private int hMul(int k) {
         double A = 0.6180339887;
-        double produto = k * A;
-        double parteFracionaria = produto - (long) produto;
-        return (int) (this.tamanhoTabela * parteFracionaria);
+        double val = k * A;
+        double frac = val - (long) val;
+        return (int) (this.tamanhoM * frac);
     }
 
     // dobramento somando de 3 em 3
-    private int hashDobramento(int k) {
+    private int hFold(int k) {
         int soma = 0;
         int temp = k;
-
         if (temp < 0) temp = temp * -1;
 
         while (temp > 0) {
@@ -69,208 +70,205 @@ public class tabelahashrecuperacao {
             temp = temp / 1000;
         }
 
-        int idx = soma % this.tamanhoTabela;
+        int idx = soma % this.tamanhoM;
         if (idx < 0) idx = idx * -1;
         return idx;
     }
 
     // ajuda a chamar a função certa
-    private int calcularHash(int k, int tipoFunc) {
-        if (tipoFunc == FUNC_DIV) return hashDivisao(k);
-        if (tipoFunc == FUNC_MUL) return hashMultiplicacao(k);
-        return hashDobramento(k);
+    private int calcHash(int k, int tipo) {
+        if (tipo == TIPO_DIV) return hDiv(k);
+        if (tipo == TIPO_MUL) return hMul(k);
+        return hFold(k);
+    }
+
+
+    // formata o numero na mao pra nao usar biblioteca proibida tipo String.format
+    private String formataNum(double valor) {
+        long parteInt = (long) valor;
+        double resto = valor - parteInt;
+        long parteDec = (long) (resto * 10000);
+
+        if (parteDec < 0) parteDec = parteDec * -1;
+
+        String sDec = "" + parteDec;
+        if (parteDec < 10) sDec = "000" + parteDec;
+        else if (parteDec < 100) sDec = "00" + parteDec;
+        else if (parteDec < 1000) sDec = "0" + parteDec;
+
+        return parteInt + "." + sDec;
     }
 
     // serve pra zerar o vetor e reinicia o contador
-    private void inicializarTabela(int m) {
-        this.tamanhoTabela = m;
+    private void iniciar(int m) {
+        this.tamanhoM = m;
         this.tabela = new No[m];
 
-        int i = 0;
-        while (i < m) {
+        for(int i = 0; i < m; i++) {
             this.tabela[i] = null;
-            i = i + 1;
         }
 
-        this.totalColisoesTabela = 0;
-        this.totalColisoesLista = 0;
-        this.somaHashPrimeiros10 = 0;
-        this.contadorInsercao = 0;
+        this.colsTabela = 0;
+        this.colsLista = 0;
+        this.somaCheck = 0;
+        this.contInsercao = 0;
     }
 
     // no final da lista ele adiciona o valor chave e conta se deu alguma colisao
-    private void inserir(int k, int tipoFunc) {
-        int h = calcularHash(k, tipoFunc);
+    private void inserir(int k, int tipo) {
+        int h = calcHash(k, tipo);
 
-        if (this.contadorInsercao < 10) {
-            this.somaHashPrimeiros10 = this.somaHashPrimeiros10 + h;
+        if (this.contInsercao < 10) {
+            this.somaCheck = this.somaCheck + h;
         }
-        this.contadorInsercao = this.contadorInsercao + 1;
+        this.contInsercao = this.contInsercao + 1;
 
         No novo = new No(k);
 
         if (this.tabela[h] == null) {
             this.tabela[h] = novo;
         } else {
-            this.totalColisoesTabela = this.totalColisoesTabela + 1;
+            this.colsTabela = this.colsTabela + 1;
 
             No atual = this.tabela[h];
+
+            int saltos = 0;
             while (atual.proximo != null) {
-                this.totalColisoesLista = this.totalColisoesLista + 1;
+                saltos = saltos + 1;
                 atual = atual.proximo;
             }
-            this.totalColisoesLista = this.totalColisoesLista + 1;
+            this.colsLista = this.colsLista + saltos + 1;
 
             atual.proximo = novo;
         }
     }
 
     // percorre a lista pra achar o valor e conta quantas comparações ele fez
-    private boolean buscar(int k, int tipoFunc) {
-        int h = calcularHash(k, tipoFunc);
+    private boolean buscar(int k, int tipo) {
+        int h = calcHash(k, tipo);
         No atual = this.tabela[h];
-
-        long comps = 0;
-        boolean achou = false;
+        long c = 0;
+        boolean achei = false;
 
         while (atual != null) {
-            comps = comps + 1;
+            c = c + 1;
             if (atual.chave == k) {
-                achou = true;
+                achei = true;
                 break;
             }
             atual = atual.proximo;
         }
 
-        if (achou) {
-            this.comparacoesHits = this.comparacoesHits + comps;
-        } else {
-            this.comparacoesMisses = this.comparacoesMisses + comps;
-        }
+        if (achei) this.cmpHits = this.cmpHits + c;
+        else this.cmpMisses = this.cmpMisses + c;
 
-        return achou;
+        return achei;
     }
 
     public void executar() {
-        // usei arrays pra facilitar o loop
-        int[] tamanhos = {m1, m2, m3};
-        int[] datasets = {n1, n2, n3};
+        int[] tams = {m1, m2, m3};
+        int[] datas = {n1, n2, n3};
         long[] seeds = {seed1, seed2, seed3};
-        int[] funcoes = {FUNC_DIV, FUNC_MUL, FUNC_FOLD};
-        String[] nomesFunc = {"H_DIV", "H_MUL", "H_FOLD"};
+        int[] funcs = {TIPO_DIV, TIPO_MUL, TIPO_FOLD};
+        String[] tags = {"H_DIV", "H_MUL", "H_FOLD"};
 
         System.out.println("m,n,func,seed,ins_ms,coll_tbl,coll_lst,find_ms_hits,find_ms_misses,cmp_hits,cmp_misses,checksum");
 
-        for (int im = 0; im < 3; im++) {
-            int m = tamanhos[im];
+        for (int m : tams) {
+            for (int n : datas) {
+                for (int f = 0; f < 3; f++) {
+                    String tag = tags[f];
 
-            for (int in = 0; in < 3; in++) {
-                int n = datasets[in];
+                    for (long s : seeds) {
+                        System.out.println(tag + " " + m + " " + s);
 
-                for (int ifunc = 0; ifunc < 3; ifunc++) {
-                    int func = funcoes[ifunc];
-                    String nomeFunc = nomesFunc[ifunc];
-
-                    for (int iseed = 0; iseed < 3; iseed++) {
-                        long seed = seeds[iseed];
-
-                        System.out.println("regra auditoria: Iniciando " + nomeFunc + " m=" + m + " seed=" + seed);
-
-                        realizarTeste(m, n, func, nomeFunc, seed);
+                        rodarTeste(m, n, funcs[f], tag, s);
                     }
                 }
             }
         }
 
-        // coloquei aqui as frases que precisa ter no relatorio
         System.out.println("RELATÓRIO - METODOLOGIA:");
         System.out.println("Distribuições mais uniformes reduzem o custo médio no encadeamento separado.");
     }
 
     // roda um teste 5 vezes pra poder pegar a media de tempo e imprimir a linha que pegou
-    private void realizarTeste(int m, int n, int func, String nomeFunc, long seed) {
-
-        int[] chavesInseridas = new int[n];
-        Random gerador = new Random(seed);
+    private void rodarTeste(int m, int n, int tipo, String tag, long seed) {
+        int[] dados = new int[n];
+        Random rnd = new Random(seed);
 
         for (int i = 0; i < n; i++) {
-            int chave = 100000000 + gerador.nextInt(900000000);
-            chavesInseridas[i] = chave;
+            dados[i] = 100000000 + rnd.nextInt(900000000);
         }
 
-        long somaTempoIns = 0;
-        long somaTempoBuscaHits = 0;
-        long somaTempoBuscaMisses = 0;
+        long tIns = 0;
+        long tHits = 0;
+        long tMiss = 0;
 
-        long capCollTbl = 0;
-        double capCollLst = 0;
-        long capCmpHits = 0;
-        long capCmpMisses = 0;
-        long capChecksum = 0;
+        long sumColTab = 0;
+        long sumColLst = 0;
+        long ultCheck = 0;
+        long sumCmpHit = 0;
+        long sumCmpMiss = 0;
 
-        for (int rep = 0; rep < 5; rep++) {
-
-            inicializarTabela(m);
-
-            long inicioIns = System.nanoTime();
-            for (int i = 0; i < n; i++) {
-                inserir(chavesInseridas[i], func);
-            }
-            long fimIns = System.nanoTime();
-            somaTempoIns = somaTempoIns + (fimIns - inicioIns);
-
-            capCollTbl = this.totalColisoesTabela;
-            capCollLst = (double) this.totalColisoesLista / n;
-            capChecksum = this.somaHashPrimeiros10 % 1000003;
-
-            this.comparacoesHits = 0;
-            int tamBuscaMetade = n / 2;
+        for (int r = 0; r < 5; r++) {
+            iniciar(m);
 
             long t0 = System.nanoTime();
-            for (int i = 0; i < tamBuscaMetade; i++) {
-                buscar(chavesInseridas[i], func);
-            }
-            somaTempoBuscaHits += (System.nanoTime() - t0);
-            capCmpHits = this.comparacoesHits / tamBuscaMetade;
+            for (int k : dados) inserir(k, tipo);
+            tIns = tIns + (System.nanoTime() - t0);
 
-            this.comparacoesMisses = 0;
+            sumColTab = sumColTab + this.colsTabela;
+            sumColLst = sumColLst + this.colsLista;
+            ultCheck = this.somaCheck % 1000003;
 
-            int[] missesSet = new int[tamBuscaMetade];
-            Random geradorMisses = new Random(seed + rep + 100);
-            for(int k=0; k<tamBuscaMetade; k++) missesSet[k] = 100000000 + geradorMisses.nextInt(900000000);
+            this.cmpHits = 0;
+            int metade = n / 2;
 
             t0 = System.nanoTime();
-            for (int i = 0; i < tamBuscaMetade; i++) {
-                buscar(missesSet[i], func);
-            }
-            somaTempoBuscaMisses += (System.nanoTime() - t0);
-            capCmpMisses = this.comparacoesMisses / tamBuscaMetade;
+            for(int i=0; i<metade; i++) buscar(dados[i], tipo);
+            tHits = tHits + (System.nanoTime() - t0);
+
+            sumCmpHit = sumCmpHit + this.cmpHits;
+
+            this.cmpMisses = 0;
+            int[] misses = new int[metade];
+            Random r2 = new Random(seed + r + 555);
+            for(int i=0; i<metade; i++) misses[i] = 100000000 + r2.nextInt(900000000);
+
+            t0 = System.nanoTime();
+            for(int k : misses) buscar(k, tipo);
+            tMiss = tMiss + (System.nanoTime() - t0);
+
+            sumCmpMiss = sumCmpMiss + this.cmpMisses;
 
             tabela = null;
         }
 
-        double mediaInsMs = (somaTempoIns / 5.0) / 1000000.0;
-        double mediaFindHitsMs = (somaTempoBuscaHits / 5.0) / 1000000.0;
-        double mediaFindMissesMs = (somaTempoBuscaMisses / 5.0) / 1000000.0;
+        double msIns = (tIns / 5.0) / 1000000.0;
+        double msHit = (tHits / 5.0) / 1000000.0;
+        double msMiss = (tMiss / 5.0) / 1000000.0;
 
-        System.out.println(
-                m + "," +
-                        n + "," +
-                        nomeFunc + "," +
-                        seed + "," +
-                        String.format("%.4f", mediaInsMs).replace(',', '.') + "," +
-                        capCollTbl + "," +
-                        String.format("%.4f", capCollLst).replace(',', '.') + "," +
-                        String.format("%.4f", mediaFindHitsMs).replace(',', '.') + "," +
-                        String.format("%.4f", mediaFindMissesMs).replace(',', '.') + "," +
-                        capCmpHits + "," +
-                        capCmpMisses + "," +
-                        capChecksum
-        );
+        long medColTab = sumColTab / 5;
+        double medColLst = (double) sumColLst / 5 / n;
+        double medCmpHit = (double) sumCmpHit / 5 / (n/2);
+        double medCmpMiss = (double) sumCmpMiss / 5 / (n/2);
+
+        String linha = m + "," + n + "," + tag + "," + seed + "," +
+                formataNum(msIns) + "," +
+                medColTab + "," +
+                formataNum(medColLst) + "," +
+                formataNum(msHit) + "," +
+                formataNum(msMiss) + "," +
+                formataNum(medCmpHit) + "," +
+                formataNum(medCmpMiss) + "," +
+                ultCheck;
+
+        System.out.println(linha);
     }
 
     public static void main(String[] args) {
-        tabelahashrecuperacao programa = new tabelahashrecuperacao();
-        programa.executar();
+        tabelahashrecuperacao prog = new tabelahashrecuperacao();
+        prog.executar();
     }
 }
